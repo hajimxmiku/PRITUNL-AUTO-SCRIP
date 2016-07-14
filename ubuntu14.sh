5a7ab68c531ccf02bc87f904899853b16730bc74
DrCyber <muhdnajmiey89@gmail.com>
2016-07-15 02:15:26 +0800

    Update ubuntu14.sh

--- ubuntu14.sh
+++ ubuntu14.sh
@@ -3,15 +3,14 @@
 # go to root
 cd
 
-# Install Pritunl
-#!/bin/bash
-echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.0.list
-echo "deb http://repo.pritunl.com/stable/apt trusty main" > /etc/apt/sources.list.d/pritunl.list
-apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7F0CEB10
-apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv CF8E292A
-apt-get --assume-yes update
-apt-get --assume-yes upgrade
-apt-get --assume-yes install pritunl mongodb-org
+#!/bin/bash  
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.0.list  
+echo "deb http://repo.pritunl.com/stable/apt trusty main" > /etc/apt/sources.list.d/pritunl.list  
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7F0CEB10 
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv CF8E292A 
+apt-get -y update 
+apt-get -y upgrade  
+apt-get -y install pritunl mongodb-org 
 service pritunl start
 
 # Install Squid
