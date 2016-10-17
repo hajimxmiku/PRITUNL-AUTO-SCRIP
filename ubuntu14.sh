780ffaa8c1c40d74dd877f3a9112409cbf2cabd3
drcyber96 <muhdnajmiey89@gmail.com>
2016-10-17 17:18:19 +0800

    Update ubuntu14.sh

--- ubuntu14.sh
+++ ubuntu14.sh
@@ -21,10 +21,7 @@ MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*
 sed -i s/xxxxxxxxx/$MYIP/g /etc/squid3/squid.conf;
 service squid3 restart
 
-# Enable Firewall
-sudo ufw allow 22,80,81,222,443,8080,9700,60000/tcp
-sudo ufw allow 22,80,81,222,443,8080,9700,60000/udp
-sudo yes | ufw enable
+
 
 # Change to Time GMT+8
 ln -fs /usr/share/zoneinfo/Asia/Kuala_Lumpur /etc/localtime
