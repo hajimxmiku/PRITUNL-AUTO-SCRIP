6eae25e016ef9cb76eded99cac2b0599354fd9df
hajimxmiku <anakhandsomee@gmail.com>
2016-10-19 00:18:06 +0800

    Update ubuntu14-pritunl.sh
    Squid with a abuse protection

--- ubuntu14-pritunl.sh
+++ ubuntu14-pritunl.sh
@@ -16,7 +16,7 @@ service pritunl start
 # Install Squid
 apt-get -y install squid3
 cp /etc/squid3/squid.conf /etc/squid3/squid.conf.orig
-wget -O /etc/squid3/squid.conf "https://raw.githubusercontent.com/drcyber96/PRITUNL-AUTO-SCRIP/master/conf/squid.conf" 
+wget -O /etc/squid3/squid.conf "https://raw.github.com/hajimxmiku/PRITUNL-AUTO-SCRIP/blob/Trial/squid(anti-abuse).conf" 
 MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | grep -v '192.168'`;
 sed -i s/xxxxxxxxx/$MYIP/g /etc/squid3/squid.conf;
 service squid3 restart
